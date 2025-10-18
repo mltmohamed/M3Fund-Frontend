c07c1be79958c5e626936fa97ca7d3b605b4b984
Mohamed Traore <traoremohamed7212@gmail.com>
2025-10-18 21:45:42 +0000

    la page dashboard responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PoteurDashboard } from './porteur/poteur-dashboard/poteur-dashboard';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', component: App }, // page d'accueil
+{ path: '**', redirectTo: '' },
+{path:'/dashboard', component:PoteurDashboard}// redirige toute URL inconnue vers l'accueil
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./porteur/navbar/navbar";
 import { Sidebar } from "./porteur/sidebar/sidebar";
-import { CardProject } from "./porteur/card-project/card-project";
-import { CardCampaign } from "./porteur/card-campaign/card-campaign";
+import { PoteurDashboard } from "./porteur/poteur-dashboard/poteur-dashboard";
 import { Footer } from "./porteur/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Sidebar, CardProject, CardCampaign, Footer],
+  imports: [RouterOutlet, Navbar, Sidebar, PoteurDashboard, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
